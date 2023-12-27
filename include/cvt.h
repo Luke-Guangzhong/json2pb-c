3f6761337afe3db2fd2b39d66f28699563d59295
luke_guangzhong <luke_guangzhong@petalmail.com>
2023-12-27 11:24:28 +0000

    update include way

--- include/cvt.h
+++ include/cvt.h
@@ -2,7 +2,7 @@
 #define __CVT_H__
 
 #include "cJSON.h"
-#include <protobuf-c.h>
+#include <protobuf-c/protobuf-c.h>
 
 int cvt_json_2_pb(ProtobufCMessage* msg, cJSON* root);
 int cvt_pb_2_json(ProtobufCMessage* msg, cJSON* root);
