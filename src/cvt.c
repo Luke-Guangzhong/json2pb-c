0d2657f7634631fe0f888aa019d4a2aa569e1b66
luke_guangzhong <luke_guangzhong@petalmail.com>
2023-12-28 01:14:37 +0000

    resort switch case

--- src/cvt.c
+++ src/cvt.c
@@ -40,48 +40,6 @@ cvt_json_2_pb(ProtobufCMessage* msg, cJSON* root)
             }
             break;
 
-        case PROTOBUF_C_TYPE_BOOL:
-            DEBUG("set %s to %s.(bool)%s\n", cJSON_Print(item), msg->descriptor->name, field_desc->name);
-            if (cJSON_IsBool(item)) {
-                cJSON_IsTrue(item) ? ((*(bool*)((void*)msg + field_desc->offset)) = true)
-                                   : ((*(bool*)((void*)msg + field_desc->offset)) = false);
-            } else {
-                ERROR("JSON field %s is not a bool\n", field_desc->name);
-            }
-            break;
-
-        case PROTOBUF_C_TYPE_ENUM:
-            DEBUG("set %s to %s(enum)%s\n", cJSON_Print(item), msg->descriptor->name, field_desc->name);
-            if (cJSON_IsString(item)) {
-                if (NULL != cJSON_GetStringValue(item) && strlen(cJSON_GetStringValue(item)) > 0) {
-                    *(int*)((void*)msg + field_desc->offset) =
-                        protobuf_c_enum_descriptor_get_value_by_name(field_desc->descriptor, cJSON_GetStringValue(item))
-                            ->value;
-                } else {
-                    ERROR("JSON field %s is empty string\n", field_desc->name);
-                }
-            } else if (cJSON_IsNumber(item)) {
-                *(int*)((void*)msg + field_desc->offset) = (int)item->valuedouble;
-            } else {
-                ERROR("JSON field %s is not valid\n", field_desc->name);
-            }
-            break;
-
-        case PROTOBUF_C_TYPE_STRING:
-            DEBUG("set %s to (string)%s\n", cJSON_Print(item), field_desc->name);
-            if (cJSON_IsString(item)) {
-                if (NULL != cJSON_GetStringValue(item) && strlen(cJSON_GetStringValue(item)) > 0) {
-                    if (asprintf((char**)((void*)msg + field_desc->offset), "%s", cJSON_GetStringValue(item)) < 0) {
-                        goto ERROR_EXIT_POINT;
-                    }
-                } else {
-                    ERROR("JSON field %s is empty string\n", field_desc->name);
-                }
-            } else {
-                ERROR("JSON field %s is not a string\n", field_desc->name);
-            }
-            break;
-
         case PROTOBUF_C_TYPE_INT64:
         case PROTOBUF_C_TYPE_SINT64:
         case PROTOBUF_C_TYPE_SFIXED64:
@@ -138,6 +96,48 @@ cvt_json_2_pb(ProtobufCMessage* msg, cJSON* root)
                 *(double*)((void*)msg + field_desc->offset) = strtod(cJSON_GetStringValue(item), NULL);
             } else {
                 ERROR("JSON field %s is not a number\n", field_desc->name);
+            }
+            break;
+
+        case PROTOBUF_C_TYPE_BOOL:
+            DEBUG("set %s to %s.(bool)%s\n", cJSON_Print(item), msg->descriptor->name, field_desc->name);
+            if (cJSON_IsBool(item)) {
+                cJSON_IsTrue(item) ? ((*(bool*)((void*)msg + field_desc->offset)) = true)
+                                   : ((*(bool*)((void*)msg + field_desc->offset)) = false);
+            } else {
+                ERROR("JSON field %s is not a bool\n", field_desc->name);
+            }
+            break;
+
+        case PROTOBUF_C_TYPE_ENUM:
+            DEBUG("set %s to %s(enum)%s\n", cJSON_Print(item), msg->descriptor->name, field_desc->name);
+            if (cJSON_IsString(item)) {
+                if (NULL != cJSON_GetStringValue(item) && strlen(cJSON_GetStringValue(item)) > 0) {
+                    *(int*)((void*)msg + field_desc->offset) =
+                        protobuf_c_enum_descriptor_get_value_by_name(field_desc->descriptor, cJSON_GetStringValue(item))
+                            ->value;
+                } else {
+                    ERROR("JSON field %s is empty string\n", field_desc->name);
+                }
+            } else if (cJSON_IsNumber(item)) {
+                *(int*)((void*)msg + field_desc->offset) = (int)item->valuedouble;
+            } else {
+                ERROR("JSON field %s is not valid\n", field_desc->name);
+            }
+            break;
+
+        case PROTOBUF_C_TYPE_STRING:
+            DEBUG("set %s to (string)%s\n", cJSON_Print(item), field_desc->name);
+            if (cJSON_IsString(item)) {
+                if (NULL != cJSON_GetStringValue(item) && strlen(cJSON_GetStringValue(item)) > 0) {
+                    if (asprintf((char**)((void*)msg + field_desc->offset), "%s", cJSON_GetStringValue(item)) < 0) {
+                        goto ERROR_EXIT_POINT;
+                    }
+                } else {
+                    ERROR("JSON field %s is empty string\n", field_desc->name);
+                }
+            } else {
+                ERROR("JSON field %s is not a string\n", field_desc->name);
             }
             break;
 
